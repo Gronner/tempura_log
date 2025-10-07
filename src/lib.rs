a18a37c080808ed35b05c8c5fb4d9189093add3a
Felix Braeunling <gronner@mailbox.org>
2025-10-07 23:14:24 +0200

    feat: setup minimal logger

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,47 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use log::{LevelFilter, Log};
+
+pub struct TempuraLogger;
+
+impl TempuraLogger {
+    pub fn init() {
+        log::set_boxed_logger(Box::new(TempuraLogger))
+            .map(|()| log::set_max_level(LevelFilter::Info))
+            .unwrap();
+    }
+}
+
+impl Log for TempuraLogger {
+    fn enabled(&self, _metadata: &log::Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &log::Record) {
+        if !self.enabled(record.metadata()) {
+            return;
+        }
+        println!(
+            "[{}] {}: {}",
+            record.level(),
+            record.target(),
+            record.args()
+        );
+    }
+
+    fn flush(&self) {}
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use log::info;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn logging_is_executed() {
+        TempuraLogger::init();
+        info!("Test log");
+        log::logger().flush();
     }
 }
